a7a5e250a85afe73d829bd5cc3b4b35d0d74cb4b
Blen <blenhadgu@computingforall.org>
2021-08-12 14:59:59 -0700

    added script tag to the html page added javascript

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "Blen";
+let lastName = "Hadgu";
+// You cannot register constant variable!
+const age = "16";
+// Variable reassignment
+
+firstName = "Bakugou's"
+lastName = "Wife"
+
+console.log('Hello '   + firstName + ' ' + lastName + '!' + ' you are '  + age + ' years old!');
